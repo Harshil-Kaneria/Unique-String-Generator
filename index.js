7f600370b56d4e51b11735371da91270bdf888d0
Harshil Kaneria <36322779+Harshil-Kaneria@users.noreply.github.com>
2021-01-15 09:54:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Developed By Harshil Kaneria
-var UniqueStringGenerator = () => {
+var UniqueString = () => {
     return Buffer.from((Math.floor(Math.random() * 1000) + 1).toString()+"_"+Date.now()+"_"+(Math.floor(Math.random() * 1000) + 1).toString()).toString('base64'); // Basically This is Return Encoding in Base64 of RandomNumber(1-1000)+"_"+CurrentTimeInMilliSecond +"_"+RandomNumber(1-1000)
-}; 
+};
 
-module.exports = UniqueStringGenerator;
+var UniqueNumber = () => {
+    return (Math.floor(Math.random() * 1000) + 1).toString()+Date.now()+(Math.floor(Math.random() * 1000) + 1).toString(); // Basically This is Return RandomNumber(1-1000)+CurrentTimeInMilliSecond+RandomNumber(1-1000)
+};
+
+var UniqueStringId = () => {
+    return Buffer.from((Math.floor(Math.random() * 1000) + 1).toString()).toString('base64')+"-"+Buffer.from(Date.now().toString()).toString('base64')+"-"+Buffer.from((Math.floor(Math.random() * 1000) + 1).toString()).toString('base64'); // Basically This is Return Encoding in Base64 of RandomNumber(1-1000)+"-"+Encoding in Base64 of CurrentTimeInMilliSecond +"-"+Encoding in Base64 of RandomNumber(1-1000)
+};
+
+var UniqueNumberId = () => {
+    return (Math.floor(Math.random() * 1000) + 1).toString()+"-"+Date.now()+"-"+(Math.floor(Math.random() * 1000) + 1).toString(); // Basically This is Return RandomNumber(1-1000)+"-"+CurrentTimeInMilliSecond +"-"+RandomNumber(1-1000)
+};
+
+module.exports = {UniqueString,UniqueNumber,UniqueStringId,UniqueNumberId};
